c91efc28ad0053d852d456af85be5e67470e8308
Jan Kadlec <jan.kadlec@nic.cz>
2015-03-31 18:58:59 +0200

    zone-api: previous workaround for namedb-trie.

--- src/knot/zone/zone-tree.c
+++ src/knot/zone/zone-tree.c
@@ -21,6 +21,7 @@
 #include "libknot/internal/macros.h"
 #include "libknot/internal/namedb/namedb_lmdb.h"
 #include "libknot/internal/namedb/namedb_trie.h"
+#include "libknot/packet/wire.h"
 
 enum get_op {
 	GET_EQ,
@@ -28,9 +29,9 @@ enum get_op {
 	GET_NEXT
 };
 
-static namedb_val_t fill_db_val(const knot_dname_t *dname, uint8_t *data)
+static namedb_val_t fill_db_val(uint8_t lf[KNOT_DNAME_MAXLEN],
+                                const knot_dname_t *dname)
 {
-	uint8_t lf[KNOT_DNAME_MAXLEN];
 	knot_dname_lf(lf, dname, NULL);
 	namedb_val_t val = { .data = lf + 1, .len = *lf };
 	return val;
@@ -66,7 +67,11 @@ size_t zone_tree_weight(const zone_tree_t* tree)
 
 bool zone_tree_is_empty(const zone_tree_t *tree)
 {
-	return zone_tree_weight(tree) == 0;
+	if (tree) {
+		return zone_tree_weight(tree) == 0;
+	} else {
+		return true;
+	}
 }
 
 int zone_tree_insert(zone_tree_t *tree, zone_node_t *node)
@@ -76,15 +81,14 @@ int zone_tree_insert(zone_tree_t *tree, zone_node_t *node)
 	}
 
 	namedb_txn_t tx;
-	int ret = tree->api->txn_begin(tree, &tx, 0);
+	int ret = tree->api->txn_begin(tree->db, &tx, 0);
 	if (ret != KNOT_EOK) {
 		return ret;
 	}
 
 	assert(tree && node && node->owner);
 	uint8_t lf[KNOT_DNAME_MAXLEN];
-
-	namedb_val_t key = fill_db_val(node->owner, lf);
+	namedb_val_t key = fill_db_val(lf, node->owner);
 	namedb_val_t val = { .data = node, .len = sizeof(node) };
 
 	ret = tree->api->insert(&tx, &key, &val, 0);
@@ -104,15 +108,12 @@ static zone_node_t *get_node(zone_tree_t *tree, const knot_dname_t *owner, unsig
 
 	uint8_t lf[KNOT_DNAME_MAXLEN];
 	knot_dname_lf(lf, owner, NULL);
-	if (op == NAMEDB_LEQ && tree->api == namedb_trie_api()) {
-		lf[*lf - 1]--;
-	}
 
 	namedb_val_t key = { .data = lf + 1, .len = *lf };
 	namedb_val_t val = { '\0' };
 
 	namedb_txn_t tx;
-	int ret = tree->api->txn_begin(tree, &tx, NAMEDB_RDONLY);
+	int ret = tree->api->txn_begin(tree->db, &tx, NAMEDB_RDONLY);
 	if (ret != KNOT_EOK) {
 		return NULL;
 	}
@@ -142,21 +143,37 @@ zone_node_t *zone_tree_get_next(zone_tree_t *tree, const knot_dname_t *owner)
 
 zone_node_t *zone_tree_get_prev(zone_tree_t *tree, const knot_dname_t *owner)
 {
+	int op = 0;
 	if (tree->api == namedb_trie_api()) {
-		const size_t size = knot_dname_size(owner);
-		knot_dname_t less[size];
-		memcpy(less, owner, size);
-		less[size - 1] -= 1;
-		owner = less;
+#warning will this work for above apex names?
+		// Pure previous not supported by trie, we have to work around that.
+		if (*owner == '\0') {
+			// Previous for root *must* be the last name.
+			return get_node(tree, owner, NAMEDB_LAST);
+		}
+		size_t change_offset = 0;
+		const knot_dname_t *next = knot_wire_next_label(owner, NULL);
+		if (next) {
+			change_offset = (next - owner) - 1;
+		} else {
+			// Only one label.
+			change_offset = knot_dname_size(owner) - 2;
+		}
+		knot_dname_t synth_prev[KNOT_DNAME_MAXLEN];
+		knot_dname_to_wire(synth_prev, owner, sizeof(synth_prev));
+		synth_prev[change_offset] -= 1;
+
+		owner = synth_prev;
+		op = NAMEDB_LEQ;
 	} else {
-		// Only trie supported.
-		assert(0);
+		op = NAMEDB_PREV;
 	}
 
-	zone_node_t *n = get_node(tree, owner, NAMEDB_LEQ);
+	zone_node_t *n = get_node(tree, owner, op);
 	if (n) {
 		return n;
 	}
+	printf("No direct prev\n");
 
 	return get_node(tree, owner, NAMEDB_LAST);
 }
@@ -182,10 +199,10 @@ int zone_tree_remove(zone_tree_t *tree,
 
 	// We have entry to delete.
 	namedb_txn_t tx;
-	tree->api->txn_begin(tree, &tx, 0);
+	tree->api->txn_begin(tree->db, &tx, 0);
 
 	uint8_t lf[KNOT_DNAME_MAXLEN];
-	namedb_val_t key = fill_db_val(owner, lf);
+	namedb_val_t key = fill_db_val(lf, owner);
 	int ret =  tree->api->del(&tx, &key);
 	if (ret != KNOT_EOK) {
 		tree->api->txn_abort(&tx);
@@ -233,20 +250,17 @@ int zone_tree_apply(zone_tree_t *tree, ztree_cb_t *cb, void *data, bool sorted)
 		return KNOT_ENOMEM;
 	}
 
-	zone_node_t *node = get_node_from(it, tree->api);
-	while (node) {
+	do {
+		zone_node_t *node = get_node_from(it, tree->api);
+		assert(node);
 		int ret = cb(node, data);
-		if (ret == KNOT_EOK) {
-			tree->api->iter_next(it);
-		}
-
 		if (ret != KNOT_EOK) {
 			tree->api->txn_abort(&tx);
 			tree->api->iter_finish(it);
 			return ret;
 		}
-		node = get_node_from(it, tree->api);
-	}
+		it = tree->api->iter_next(it);
+	} while (it);
 
 	return KNOT_EOK;
 }
