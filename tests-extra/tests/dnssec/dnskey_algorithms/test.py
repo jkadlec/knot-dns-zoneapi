56bf6da9b6593f69dd3a4ec4fc6a6a0ad21066a1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-18 21:05:01 +0000

    Adding tarfile member sanitization to extractall()

--- tests-extra/tests/dnssec/dnskey_algorithms/test.py
+++ tests-extra/tests/dnssec/dnskey_algorithms/test.py
@@ -36,7 +36,26 @@ knot.dnssec_enable = True
 
 keys_archive = os.path.join(t.data_dir, "keys.tgz")
 with tarfile.open(keys_archive, "r:*") as tar:
-    tar.extractall(knot.keydir)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar, knot.keydir)
 
 # setup zones
 
